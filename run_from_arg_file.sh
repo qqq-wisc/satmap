e4e34943363817850b0642163eef89c381179cc2
Abtin Molavi <amolavi@wisc.edu>
2022-07-28 15:07:33 -0500

    rm sem

--- run_from_arg_file.sh
+++ run_from_arg_file.sh
@@ -1,4 +1,2 @@
 while read i ; do 
-sem -j+0
-    "python3" "src/experiment_runner.py" $i; "python3" "src/plotter.py" $2; done < $1
-sem --wait
+"python3" "src/experiment_runner.py" $i; "python3" "src/plotter.py" $2; done < $1
